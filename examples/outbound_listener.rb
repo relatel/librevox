2536c7c001b58282f7dc5955b62f3eb8e29fdd9e
Henrik Hauge Bjørnskov <19725+henrikbjorn@users.noreply.github.com>
2026-02-24 10:25:26 +0100

    Update outbound example to blocking style
    Replace nested callback style with sequential blocking calls to match
    the new async API.

--- examples/outbound_listener.rb
+++ examples/outbound_listener.rb
@@ -8,23 +8,22 @@ class MyApp < Librevox::Listener::Outbound
   end
 
   def session_initiated
-    answer do
-      playback "/path/to/file.wav" do
-        # For apps that read input (play_and_get_digits, read), pass a block
-        # to receive the result.
-        play_and_get_digits "/sounds/enter-digit.wav", "/sounds/wrong-digit.wav" do |digit|
-          # Set channel variables
-          set "playback_terminators", "#"
+    answer
+    playback "/path/to/file.wav"
 
-          # For apps not yet wrapped by a named helper, call
-          # `application` directly:
-          #
-          #   application "record", "/recordings/#{digit}.wav"
-          #
-          bridge "sofia/foo/bar", "sofia/foo/baz"
-        end
-      end
-    end
+    # For apps that read input (play_and_get_digits, read), the return
+    # value contains the result.
+    digit = play_and_get_digits "/sounds/enter-digit.wav", "/sounds/wrong-digit.wav"
+
+    # Set channel variables
+    set "playback_terminators", "#"
+
+    # For apps not yet wrapped by a named helper, call
+    # `application` directly:
+    #
+    #   application "record", "/recordings/#{digit}.wav"
+    #
+    bridge "sofia/foo/bar", "sofia/foo/baz"
   end
 end
 
